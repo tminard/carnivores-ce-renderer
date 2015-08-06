40f496c0390ae68571217429a2c6a1828d59da34
Tyler Minard <adelphospro@gmail.com>
2015-08-06 14:08:34 -0400

    Initial Commit

---
+++ CE Character Lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CE Character Lab
+//
+//  Created by Tyler Minard on 8/6/15.
+//  Copyright (c) 2015 Tyler Minard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}

6488c30bef0dbe00c4c053ecb4bf3581403f0515
Tyler Minard <adelphospro@gmail.com>
2025-08-20 00:18:13 -0400

    feat: make sky geo configurable. Flat vs dome.

--- src/C2Sky.h
+++ src/C2Sky.h
@@ -22,6 +22,11 @@
 
 #include "shader_program.h"
 
+enum class SkyType {
+  FLAT,
+  DOME
+};
+
 class CETexture;
 class ShaderProgram;
 class Tga;
@@ -34,6 +39,8 @@ class C2Sky
 private:
   const float SKY_DISTANCE = 4000.f;  // Reduced distance to prevent edge artifacts
   const float SKY_HEIGHT = 250.f;     // Lower cloud plane height
+  const float DOME_RADIUS = 3000.f;   // Dome radius for cloud dome
+  const int DOME_SEGMENTS = 32;       // Number of dome segments (resolution)
 
   std::unique_ptr<ShaderProgram> m_shader;
 
@@ -41,15 +48,26 @@ private:
   GLuint m_vertex_array_buffer;
   GLuint m_cube_texture;
 
+  // Sky dome for clouds
+  GLuint m_dome_vertex_array_object;
+  GLuint m_dome_vertex_buffer;
+  GLuint m_dome_uv_buffer;
+  std::vector<float> m_dome_vertices;
+  std::vector<float> m_dome_uvs;
+  int m_dome_vertex_count;
+
   std::unique_ptr<CETexture> m_texture;
   std::unique_ptr<ShaderProgram> m_cloud_shader;
   GLuint m_cloud_uv_buffer;
   glm::vec4 m_sky_rgba;
   double last_cloud_time;
   float last_cloud_start;
+  SkyType m_sky_type;
 
   void loadIntoHardwareMemory();
   void loadTextureIntoMemory();
+  void generateDomeGeometry();
+  SkyType readSkyTypeFromConfig();
 public:
   C2Sky(std::ifstream& instream, std::filesystem::path shaderPath);
   C2Sky(std::unique_ptr<CETexture> sky_texture, std::filesystem::path shaderPath);
